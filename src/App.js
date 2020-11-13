b216f375266b53f44a0fa363ff6f9fd823525513
u2017049 <73095318+u2017049@users.noreply.github.com>
2020-11-13 10:29:16 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Ahmed Shamas!
+    DevOps: Assignment 4
         </p>
         <a
           className="App-link"
